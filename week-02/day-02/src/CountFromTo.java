c8513281c55123c12fb9f0041cf6234932b4f776
Lengyel Norbert <lengyelnorbert@hotmail.com>
2017-03-21 17:43:26 +0100

    exercises 23, 24 fully done, 25 partlÿ

---
+++ week-02/day-02/src/CountFromTo.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class CountFromTo {
+// Create a program that asks for two numbers
+// If the second number is not bigger than the first one it should print:
+// "The second number should be bigger"
+//
+// If it is bigger it should count from the first number to the second by one
+  public static void main(String[] args) {
+
+    Scanner thisInput = new Scanner(System.in);
+    System.out.println("Give me the first number pls:");
+    int firstNumber = thisInput.nextInt();
+
+    System.out.println("Give me the second number pls:");
+    int secondNumber = thisInput.nextInt();
+
+    if (secondNumber <= firstNumber){
+      System.out.println("The second number should be bigger");
+    }
+    else {
+      for (int printNumber )
+    }
+
+  }
+}
+
+
---
+++ week-02/day-02/src/PrintEven.java
@@ -0,0 +1,13 @@
+// Create a program that prints all the even numbers between 0 and 500
+public class PrintEven {
+
+  public static void main(String[] args) {
+    int a = 0;
+    while (a < 501) {
+      System.out.println(a); // Prints the numbers from 0 to 9
+      a += 2;
+    }
+  }
+}
+
+
