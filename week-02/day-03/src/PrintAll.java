629427cc3e5848a726c2f8b46f99e4bcb97e08d9
Lengyel Norbert <lengyelnorbert@hotmail.com>
2017-03-22 13:04:06 +0100

    exercises added from Third to PrintAll

---
+++ week-02/day-03/src/PrintAll.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+// - Create an array variable named `af`
+//   with the following content: `[4, 5, 6, 7]`
+// - Print all the elements of `af`
+public class PrintAll {
+
+  public static void main(String[] args) {
+
+    int[] af = {4, 5, 6, 7};
+    System.out.println(Arrays.toString(af));
+
+  }
+
+}
