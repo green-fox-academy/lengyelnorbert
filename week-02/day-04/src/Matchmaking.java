f16a536fbea58c016cd90d04b2fa763b363758f7
Lengyel Norbert <lengyelnorbert@hotmail.com>
2017-03-23 15:47:28 +0100

    matchmaking extended / appendletter added

---
+++ week-02/day-04/src/Matchmaking.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+// Join the two lists by matching one girl with one boy in the order list
+// Exepected output: "Eve", "Joe", "Ashley", "Fred"...
+
+public class Matchmaking {
+
+  public static void main(String... args) {
+    ArrayList<String> girls = new ArrayList<String>(
+            Arrays.asList("Eve", "Ashley", "Bözsi", "Kat", "Jane"));
+    ArrayList<String> boys = new ArrayList<String>(
+            Arrays.asList("Joe", "Fred", "Béla", "Todd", "Neef", "Jeff"));
+    ArrayList<String> order = new ArrayList<String>();
+
+    int longerList;
+    if (boys.size() >= girls.size()) {
+      longerList = boys.size();
+    } else {
+      longerList = girls.size();
+    }
+
+    for (int i = 0; i < longerList - 1; i++) {
+      if (i <= girls.size() && i <= boys.size()) {
+        order.add(girls.get(i) + ", " + boys.get(i));
+      } else if (i <= boys.size()) {
+        order.add(boys.get(i));
+      } else if (i <= girls.size()) {
+        order.add(girls.get(i));
+
+      }
+
+    }
+
+    System.out.println(order);
+  }
+}
