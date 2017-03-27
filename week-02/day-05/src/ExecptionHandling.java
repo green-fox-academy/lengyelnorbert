eff76bc26a51fb2a28b2581c947c931d5345b92d
Lengyel Norbert <lengyelnorbert@hotmail.com>
2017-03-27 19:15:40 +0200

    misfunctions fixes

---
+++ week-02/day-05/src/ExecptionHandling.java
@@ -0,0 +1,12 @@
+/**
+ * Created by kispista on 2017.03.27..
+ */
+public class ExecptionHandling {
+
+  public static void main(String[] args) {
+
+
+    
+  }
+
+}
---
+++ week-02/day-05/src/ForEach.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by kispista on 2017.03.27..
+ */
+public class ForEach {
+  public static void main(String[] args{
+
+    ArrayList<Integer> mynumbers = new ArrayList<>();
+    for (int i = 0; i < 50; i++) {
+
+    }
+
+
+
+
+  }
+
+
+
+
+}
