666d82caf7e338db72bb0ac24ab073ace714f20f
Lengyel Norbert <lengyelnorbert@hotmail.com>
2017-03-30 10:12:51 +0200

    for loop moved out from the function

--- week-03/day-02/src/TriAngle.java
+++ week-03/day-02/src/TriAngle.java
@@ -6,28 +6,24 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class TriAngle {
 
-  public static void mainDraw(Graphics graphics){
+  public static void mainDraw(Graphics graphics) {
     // reproduce this:
     // [https://github.com/greenfox-academy/teaching-materials/blob/master/exercises/drawing/triangles/r5.png]
 
     int triAngleSideLength = 40;
     int[] topcord = {160, 30};
-    for (int i = 0; i < 160 / triAngleSideLength / 2 ; i += 2) {
-      drawTrianlge(graphics, topcord, triAngleSideLength);
-
-
-    }
-
-
-
+    topcord = drawTrianlge(graphics, topcord, triAngleSideLength);
+    // here needs those for loops to set up the top coords of all the triangles
   }
 
 
-  public static int[] drawTrianlge(Graphics graphics, int[] topcoord, int sideLength){
+
+
+  public static int[] drawTrianlge(Graphics graphics, int[] topcoord, int sideLength) {
     int[] a, b, c;
     a = topcoord;
-    b = new int[]{a[0]+sideLength/2, a[1]+(int)((Math.sqrt(3))/2*sideLength)};
-    c = new int[]{a[0]-sideLength/2, a[1]+(int)((Math.sqrt(3))/2*sideLength)};
+    b = new int[]{a[0] + sideLength / 2, a[1] + (int) ((Math.sqrt(3)) / 2 * sideLength)};
+    c = new int[]{a[0] - sideLength / 2, a[1] + (int) ((Math.sqrt(3)) / 2 * sideLength)};
     graphics.drawLine(a[0], a[1], b[0], b[1]);
     graphics.drawLine(b[0], b[1], c[0], c[1]);
     graphics.drawLine(c[0], c[1], a[0], a[1]);
@@ -35,10 +31,7 @@ public class TriAngle {
     return c;
 
 
-
   }
-
-
 
 
   //    Don't touch the code below
@@ -50,7 +43,9 @@ public class TriAngle {
     jFrame.setLocationRelativeTo(null);
     jFrame.setVisible(true);
   }
-  static class ImagePanel extends JPanel{
+
+  static class ImagePanel extends JPanel {
+
     @Override
     protected void paintComponent(Graphics graphics) {
       super.paintComponent(graphics);
