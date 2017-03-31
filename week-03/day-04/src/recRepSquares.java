ca2aed1c48887a68e33fce84759af579a0a247e8
Lengyel Norbert <lengyelnorbert@hotmail.com>
2017-03-31 09:59:09 +0200

    recRepSquares solved

--- week-03/day-04/src/recRepSquares.java
+++ week-03/day-04/src/recRepSquares.java
@@ -6,40 +6,60 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class recRepSquares {
 
-  public static void mainDraw(Graphics graphics){
 
-    int sizeToDived = 300;
-    recDraw(graphics, sizeToDived);
+
+  public static void mainDraw(Graphics graphics) {
+    graphics.setColor(new Color(255, 255, 0, 255));
+    graphics.fillRect(0, 0, 600, 600);
+    graphics.setColor(new Color(0, 0, 0, 255));
+    graphics.drawRect(0, 0, 600, 600);
+
+    fract(0, 0, 300, graphics);
+
+
+  }
+  public static void drawTheLines(int x, int y, int maxLines, Graphics graphics){
+    graphics.drawLine(x+maxLines/3, y, x+maxLines/3, y+maxLines);
+    graphics.drawLine(x+maxLines/3*2, y, x+maxLines/3*2, y+maxLines);
+    graphics.drawLine(x, y+maxLines/3, x+maxLines, y+maxLines/3);
+    graphics.drawLine(x, y+maxLines/3*2, x+maxLines, y+maxLines/3*2);
 
 
 
   }
-  public static void recDraw (Graphics graphics, int sizeToDived){
-    if (sizeToDived < 3){
+  public static void fract(int x, int y, int maxLines, Graphics graphics){
+    if (maxLines < 1){
       return;
     }
-    else{
-      graphics.drawLine();
+    else {
+      drawTheLines(x, y, maxLines, graphics);
+      fract(x+maxLines/3, y, maxLines/3, graphics);
+      fract(x, y+maxLines/3, maxLines/3, graphics);
+      fract(x+maxLines/3*2, y+maxLines/3, maxLines/3, graphics);
+      fract(x+maxLines/3, y+maxLines/3*2, maxLines/3, graphics);
+
     }
-
-
   }
+
+
+
 
 
   //    Don't touch the code below
   public static void main(String[] args) {
     JFrame jFrame = new JFrame("Drawing");
     jFrame.setSize(new Dimension(300, 300));
-    jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
     jFrame.add(new ImagePanel());
     jFrame.setLocationRelativeTo(null);
     jFrame.setVisible(true);
   }
-  static class ImagePanel extends JPanel{
+
+  static class ImagePanel extends JPanel {
+
     @Override
     protected void paintComponent(Graphics graphics) {
       super.paintComponent(graphics);
-      this.setBackground(Color.yellow);
       mainDraw(graphics);
 
     }
