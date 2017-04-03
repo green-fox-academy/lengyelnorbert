6ef018702744066dd533d9664acb24367cb5bac3
Lengyel Norbert <lengyelnorbert@hotmail.com>
2017-04-03 11:44:52 +0200

    BlogPost added, main function extended

---
+++ week-04/day-01/src/BlogPost.java
@@ -0,0 +1,15 @@
+/**
+ * Created by kispista on 2017.04.03..
+ */
+public class BlogPost {
+  String authorName;
+  String title;
+  String text;
+  String publicationDate;
+
+  public BlogPost(String authorName, String title, String text, String publicationDate){
+
+  }
+
+
+}
--- week-04/day-01/src/theMainFunction.java
+++ week-04/day-01/src/theMainFunction.java
@@ -9,17 +9,17 @@ public class theMainFunction {
     example_one.textColor = "blue";
     example_one.textOnIt = "Idea 1";
 
-    PostIt example_two = new PostIt()
+    PostIt example_two = new PostIt();
     example_two.backGroundColor = "pink";
     example_two.textColor = "black";
     example_two.textOnIt = "Awesome";
 
     PostIt example_three = new PostIt("yellow", "green", "Superb");
-
     PostIt example_four = new PostIt("blue", "gree", "text");
 
+    BlogPost bpFirst = new BlogPost("John Doe", "Lorem Ipsum", "Lorem ipsum dolor sit amet.", "2000.05.04.");
+    BlogPost bpSecond = new BlogPost("Tim Urban", "Wait but why", "A popular long-form, stick-figure-illustrated blog about almost everything.", "2010.10.10.");
+    BlogPost bpThird = new BlogPost("William Turton", "One Engineer Is Trying to Get IBM to Reckon With Trump", "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.","2017.03.28.");
 
-
-    
   }
 }
