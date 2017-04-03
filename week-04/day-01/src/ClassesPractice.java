a010de15157cdee489a83a11fad1ef6cc43bf839
Lengyel Norbert <lengyelnorbert@hotmail.com>
2017-04-03 10:58:30 +0200

    ClassesPractice added

---
+++ week-04/day-01/src/ClassesPractice.java
@@ -0,0 +1,16 @@
+
+public class ClassesPractice {
+
+  public class Bicycle_one {
+    int cadence;
+    int gear;
+    int speed;
+  }
+
+  public class FamilyMember {
+    String name;
+    int age;
+    String surname;
+  }
+
+}
