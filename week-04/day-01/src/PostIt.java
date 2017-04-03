5b52701a9420feae4000f99499aab847599a8a40
Lengyel Norbert <lengyelnorbert@hotmail.com>
2017-04-03 11:36:18 +0200

    postIt exercise done

---
+++ week-04/day-01/src/PostIt.java
@@ -0,0 +1,16 @@
+public class PostIt {
+
+  String backGroundColor;
+  String textOnIt;
+  String textColor;
+
+  public PostIt() {
+
+  }
+  public PostIt(String backGroundColor, String textOnIt, String textColor){
+    this.backGroundColor = backGroundColor;
+    this.textOnIt = textOnIt;
+    this.textColor = textColor;
+
+  }
+}
