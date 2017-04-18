c1391cce419cc9ac2777e213964d02d601169daf
Lengyel Norbert <lengyelnorbert@hotmail.com>
2017-04-18 18:32:48 +0200

    charoperation added

--- week-06/day-01/src/charoperation/CharOperation.java
+++ week-06/day-01/src/charoperation/CharOperation.java
@@ -1,9 +1,5 @@
 package charoperation;
 
-import com.sun.xml.internal.fastinfoset.util.CharArray;
-import java.util.ArrayList;
-import java.util.List;
-
 public class CharOperation implements CharSequence {
 
   String textString;
@@ -24,15 +20,19 @@ public class CharOperation implements CharSequence {
 
   @Override
   public CharSequence subSequence(int start, int end) {
-    CharSequence returnCS = textString.substring(start, end).toString();
-    return returnCS;
+    CharSequence returnCS = textString.substring(start, end);
+    String returnString = "";
+    for (int i = returnCS.length() - 1; i >= 0; i--) {
+      returnString += charAt(i);
+    }
+    return returnString;
   }
 
   @Override
   public String toString() {
     String returnString = "";
-    for (int i = textString.length() - 1; i >= 0 ; i--) {
-      returnString += charAt(i) ;
+    for (int i = textString.length() - 1; i >= 0; i--) {
+      returnString += charAt(i);
     }
     return returnString;
   }
--- week-06/day-01/src/charoperation/Main.java
+++ week-06/day-01/src/charoperation/Main.java
@@ -11,7 +11,5 @@ public class Main {
     System.out.println(testString.charAt(3));
     System.out.println(testString.toString());
 
-
   }
-
 }
