805a6e5d9cde4b32ff3ed1e8055df8f481f024f3
Lengyel Norbert <lengyelnorbert@hotmail.com>
2017-04-21 11:40:25 +0200

    file handlin methods added

--- week-06/day-04/todoapp/src/main/java/fileHandler.java
+++ week-06/day-04/todoapp/src/main/java/fileHandler.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class fileHandler {
@@ -34,11 +35,43 @@ public class fileHandler {
       e.printStackTrace();
     }
   }
+
+  public static List<String[]> readTodoFile(String todosFilePlace){
+    List<String[]> dataFromFile = new ArrayList<>();
+    try {
+      CSVReader reader = new CSVReader(new FileReader(todosFilePlace), ';');
+      dataFromFile = reader.readAll();
+    }catch (Exception e) {
+      System.out.println("Error during reading the file");
+      e.printStackTrace();
+    }
+    return dataFromFile;
+  }
+
+  public static void writeTodoFile (String todosFilePlace, List<String[]> dataToWrite){
+    try {
+      CSVWriter writer = new CSVWriter(new FileWriter(todosFilePlace), ';',
+              CSVWriter.NO_QUOTE_CHARACTER);
+      writer.writeAll(dataToWrite);
+      writer.close();
+    }catch (Exception e) {
+      System.out.println("Error during writing the file");
+      e.printStackTrace();
+    }
+  }
 }
 
-//  CSVReader reader = new CSVReader(new FileReader("data.csv"), ';');
-//  List<String[]> lines = reader.readAll();
+
+
+
+
+
 //  CSVWriter writer = new CSVWriter(new FileWriter("copydata.csv"), ';',
 //          CSVWriter.NO_QUOTE_CHARACTER);
 //writer.writeAll(lines);
 //        writer.close();
+
+
+
+//  CSVReader reader = new CSVReader(new FileReader("data.csv"), ';');
+//  List<String[]> lines = reader.readAll();
