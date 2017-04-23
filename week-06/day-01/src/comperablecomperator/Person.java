03346be04c045ffc8e38dc28223ad7cb030b3b09
Lengyel Norbert <lengyelnorbert@hotmail.com>
2017-04-23 13:53:35 +0200

    comperable comperator initial commit

---
+++ week-06/day-01/src/comperablecomperator/Person.java
@@ -0,0 +1,31 @@
+package comperablecomperator;
+
+
+public class Person {
+
+  String name;
+  int age;
+  String gender;
+
+  public void introduce() {
+    System.out.println("Hi, I'm " + name + ", " + age + "year old " + gender + ".");
+  }
+
+  public void getGoal() {
+    System.out.println("My goal is: Live for the moment!");
+  }
+
+  public Person(String name, int age, String gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+
+  public Person() {
+    name = "Jane Doe";
+    age = 30;
+    gender = "female";
+  }
+
+
+}
