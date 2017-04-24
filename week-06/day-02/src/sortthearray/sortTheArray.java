a57880d57431b741db421b9377674d6bc2cedbc3
Lengyel Norbert <lengyelnorbert@hotmail.com>
2017-04-24 12:07:21 +0200

    sorted arreys added

--- week-06/day-02/src/sortthearray/sortTheArray.java
+++ week-06/day-02/src/sortthearray/sortTheArray.java
@@ -1,8 +1,30 @@
 package sortthearray;
 
-/**
- * Created by kispista on 2017.04.24..
- */
+import java.util.Arrays;
+
 public class sortTheArray {
 
+  public static void main(String[] args) {
+
+    // Create arrays of Integer, Double and Character
+    Integer[] intArray = {22, 33, 55, 1, 2, 3, 4, 5};
+    Double[] doubleArray = {1.1, 2.2, 3.3, 4.4};
+    Character[] charArray = {'H', 'E', 'L', 'L', 'O'};
+
+    System.out.println("Array integerArray contains:");
+    descendingSort(intArray);   // pass an Integer array
+
+    System.out.println("\nArray doubleArray contains:");
+    descendingSort(doubleArray);   // pass a Double array
+
+    System.out.println("\nArray characterArray contains:");
+    descendingSort(charArray);   // pass a Character array
+  }
+
+
+  public static <T> void descendingSort(T[] arrayToSort) {
+//    for (T element: arrayToSort){
+    Arrays.sort(arrayToSort);
+    System.out.println(Arrays.toString(arrayToSort));
+  }
 }
