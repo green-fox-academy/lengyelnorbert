e32a0ad63adf802a75edb825ad0054c6b4956580
Lengyel Norbert <lengyelnorbert@hotmail.com>
2017-04-24 14:03:38 +0200

    sum the array finalized

--- week-06/day-02/src/sumthearray/SumTheArray.java
+++ week-06/day-02/src/sumthearray/SumTheArray.java
@@ -1,8 +1,23 @@
 package sumthearray;
 
-/**
- * Created by kispista on 2017.04.24..
- */
 public class SumTheArray {
 
+  public static void main(String[] args) {
+    Integer[] intArray = {33, 55, 1, 2, 3, 4, 5};
+    Double[] doubleArray = {1.1, 2.2, 3.3, 4.4};
+
+    System.out.println("Array integer sum:");
+    System.out.println(sumArray(intArray));
+
+    System.out.println("\nArray double sum:");
+    System.out.println(sumArray(doubleArray));
+  }
+
+  public static <T extends Number> double sumArray(T[] inputArray){
+    double sum = 0;
+    for (int i = 0; i < inputArray.length; i++) {
+      T temp = inputArray[i];
+      sum += temp.doubleValue();
+    }return sum;
+  }
 }
